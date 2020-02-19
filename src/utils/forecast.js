cfce3d890e38ac4f9572e4128d2453c4ab05e281
yakovtayar <tiary@post.bgu.ac.il>
2020-02-19 16:02:33 +0200

    Added data to forecas

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,11 @@ const forecast = (latitude, longitude, callback) =>{
         } else if(body.error){
             callback('Unable to find location', undefined)
         } else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +
+             ' degrees out. The humidity today is '+ body.daily.data[0].humidity+'.' + ' There is ' + body.currently.precipProbability +
+              '% chance of rain. The lowest temperature for today is ' +
+               body.daily.data[0].temperatureMin + ' And the highest temperature for today is ' +
+                body.daily.data[0].temperatureMax +'.')
         }       
     })
 }
